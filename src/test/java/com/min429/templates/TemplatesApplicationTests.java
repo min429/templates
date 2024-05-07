359175a6f988698b0fc7cdde4d50a34b0cb1ec70
min429 <lsm990429@gmail.com>
2024-05-07 18:49:20 +0900

    Initial commit

---
+++ src/test/java/com/min429/templates/TemplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min429.templates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
